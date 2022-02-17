580992c0423d2b456776abf1b065161aac4f3a90
larissadantier <larissa_dantier@hotmail.com>
2022-02-17 17:57:18 -0300

    Form error

--- fe/src/components/FormGroup/styles.js
+++ fe/src/components/FormGroup/styles.js
@@ -2,6 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 
+
+ small {
+  display: block;
+  margin-top: 8px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.danger.main};
+}
+
   & + & {
     margin-top: 16px;
   }
