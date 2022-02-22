ddbf772bfc158e4087d853139369a6fe8b0d7c7f
larissadantier <larissa_dantier@hotmail.com>
2022-02-22 16:14:37 -0300

    Change Port 3000 > 3001

--- api/src/index.js
+++ api/src/index.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use((error, request, response, next) => {
-  console.log('ERROR HANDLER');
   console.log(error);
   response.sendStatus(500);
 });
 
-app.listen(3000, () => console.log('🔥 Server Started http://localhost:3000'));
+app.listen(3001, () => console.log('🔥 Server Started http://localhost:3001'));
